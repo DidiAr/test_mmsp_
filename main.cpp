70f9628a48326c539173e951e616786a3ec49118
Arisova Diana <94220810+DidiAr@users.noreply.github.com>
2021-11-23 10:35:31 +0500

    11

--- main.cpp
+++ main.cpp
@@ -2,13 +2,5 @@
 using namespace std
 int main(){
 cout<<"hello"<<endl;
-cout << "bye" << endl;
-cout<< "good mood";
-  for (int i=0; i<5;i++)
-  {
-  cout<<i<<":"<<endl;
-  cout<<"i am wanna to home"<<endl;
-  cout<<"me too"<<endl;
-  }
 return 0;
 }
